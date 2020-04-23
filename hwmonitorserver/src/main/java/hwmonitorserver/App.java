3f37f60c29e6ea4e99da83ed2681523406e3553b
LUMERIIX <lukas.jenni@comlab.ch>
2020-04-23 19:55:18 +0200

    add more oshi printouts

--- hwmonitorserver/src/main/java/hwmonitorserver/App.java
+++ hwmonitorserver/src/main/java/hwmonitorserver/App.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import oshi.SystemInfo;
 import oshi.hardware.CentralProcessor;
 import oshi.hardware.HardwareAbstractionLayer;
+import oshi.util.FormatUtil;
 
 /**
  * Hello world!
@@ -20,16 +21,18 @@ public final class App {
         System.out.println("Hello World!");
         SystemInfo si = new SystemInfo();
         HardwareAbstractionLayer hal = si.getHardware();
-        //CentralProcessor.ProcessorIdentifier cpu = new CentralProcessor.ProcessorIdentifier();
         CentralProcessor cpu;
         //String cpuModel = cpu.getModel();
         System.out.println(si.getOperatingSystem());
-        System.out.println(hal.getProcessor());
+        System.out.println(hal.getProcessor().getName());
         long[] table = hal.getProcessor().getCurrentFreq();
+        for (int i = 0; i < table.length; i++) {
+            table[i] = table[i] / 1000000000;
+        }
         System.out.println(Arrays.toString(table));
-        //for(table.length)
-
-        /*System.out.println("Memory: " + FormatUtil.formatBytes(hal.getMemory().getAvailable())
-        + "/" + FormatUtil.formatBytes(hal.getMemory().getTotal())); //Mempry usage*/
+        System.out.println("Memory: " + FormatUtil.formatBytes(hal.getMemory().getAvailable()) + "/" + FormatUtil.formatBytes(hal.getMemory().getTotal())); //Mempry usage
+        System.out.println("CPU temp: " + hal.getSensors().getCpuTemperature());
+        //int[] FanSpeeds = hal.;
+        //System.out.println("Fan Speeds: " + Arrays.toString(FanSpeeds));
     }
 }
