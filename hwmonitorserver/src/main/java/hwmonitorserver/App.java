c279f2daced739c2e20cd55cd6e3c746e6e3e96c
LUMERIIX <lukas.jenni@comlab.ch>
2020-04-26 13:16:38 +0200

    hwmointor server integration (json parsable)

--- hwmonitorserver/src/main/java/hwmonitorserver/App.java
+++ hwmonitorserver/src/main/java/hwmonitorserver/App.java
@@ -1,10 +1,38 @@
 package hwmonitorserver;
 
-import java.util.Arrays;
-import oshi.SystemInfo;
-import oshi.hardware.CentralProcessor;
-import oshi.hardware.HardwareAbstractionLayer;
-import oshi.util.FormatUtil;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+//import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.json.JSONException;
+//import org.json.JSONObject;
+
+enum Index{
+  PCNAME(1),
+  CPUNAME(3),
+  CPUTOTALLOAD(5);
+  private int index; 
+  
+  // getter method 
+  public int getIndex() 
+  { 
+      return this.index; 
+  } 
+
+  // enum constructor - cannot be public or protected 
+  private Index(int index) 
+  { 
+      this.index = index; 
+  } 
+}
 
 /**
  * Hello world!
@@ -17,23 +45,31 @@ public final class App {
      * Says hello to the world.
      * @param args The arguments of the program.
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        SystemInfo si = new SystemInfo();
-        HardwareAbstractionLayer hal = si.getHardware();
-        CentralProcessor cpu;
-        //String cpuModel = cpu.getModel();
-        System.out.println(si.getOperatingSystem());
-        System.out.println(hal.getProcessor().getName());
-        long[] table = hal.getProcessor().getCurrentFreq();
-        double[] doubletable = new double[table.length];
-        for (int i = 0; i < table.length; i++) {
-            doubletable[i] = (double) table[i] / 1000000000.0;
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+          sb.append((char) cp);
         }
-        System.out.println(Arrays.toString(doubletable));
-        System.out.println("Memory: " + FormatUtil.formatBytes(hal.getMemory().getAvailable()) + "/" + FormatUtil.formatBytes(hal.getMemory().getTotal())); //Mempry usage
-        System.out.println("CPU temp: " + hal.getSensors().getCpuTemperature());
-        int[] FanSpeeds = hal.getSensors().getFanSpeeds();
-        System.out.println("Fan Speeds: " + Arrays.toString(FanSpeeds));
-    }
+        return sb.toString();
+      }
+    
+      public static JsonObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+          BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+          String jsonText = readAll(rd);
+          JsonObject json = new JsonParser().parse(jsonText).getAsJsonObject();
+          return json;
+        } finally {
+          is.close();
+        }
+      }
+    
+      public static void main(String[] args) throws IOException, JSONException {
+        JsonObject json = readJsonFromUrl("http://192.168.1.20:8085/data.json");
+        assert(json.isJsonObject());
+        System.out.println(json + "\n" + "\n" );
+      }
 }
